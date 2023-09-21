c225b5e59a5f8fa35b99092f655747bbb2b1e43f
请听下一题 <2286512681@qq.com>
2023-09-21 19:19:51 +0800

    性能分析中添加锁，避免在分析多线程时出错

--- Hazel_demo/src/Hazel/Debug/Instrumentor.h
+++ Hazel_demo/src/Hazel/Debug/Instrumentor.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <string>
-#include <chrono>
 #include <algorithm>
+#include <chrono>
 #include <fstream>
 
+#include <string>
 #include <thread>
 
 namespace Hazel {
@@ -13,7 +13,7 @@ namespace Hazel {
     {
         std::string Name;//测试的名字
         long long Start, End;//开始时间和结束时间
-        uint32_t ThreadID;//线程ID
+        std::thread::id ThreadID;//线程的ID
     };
 
     struct InstrumentationSession//会话
@@ -25,56 +25,83 @@ namespace Hazel {
     class Instrumentor
     {
     private:
+        std::mutex m_Mutex;//互斥量
         InstrumentationSession* m_CurrentSession;//当前会话
         std::ofstream m_OutputStream;//输出流
-        int m_ProfileCount;//测试的数量
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : m_CurrentSession(nullptr)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")//开始一个会话
         {
-            m_OutputStream.open(filepath);//打开输出流
-            WriteHeader();//写入头部
-            m_CurrentSession = new InstrumentationSession{ name };//创建一个会话
+            std::lock_guard lock(m_Mutex);//加锁
+            if (m_CurrentSession) {
+                // 如果已经有一个会话了，那就先结束它，然后再开始新的会话。
+                // 连续的会话之间不应该有间隔，因为我们希望它们是连续的块，而不是散乱的。
+                if (Log::GetCoreLogger())//如果日志不为空
+                {
+                    HZ_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                }
+                InternalEndSession();//结束当前会话
+            }
+
+            m_OutputStream.open(filepath);// 打开输出流
+            if(m_OutputStream.is_open()){
+                m_CurrentSession = new InstrumentationSession({ name });//创建一个新的会话
+                WriteHeader();//写入头部
+            }else{
+                if (Log::GetCoreLogger())//如果日志不为空
+                {
+                    HZ_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                }
+            }
         }
 
         void EndSession()//结束一个会话
         {
-            WriteFooter();//写入尾部
-            m_OutputStream.close();//关闭输出流
-            delete m_CurrentSession;//删除会话
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);//加锁
+            InternalEndSession();//结束当前会话
         }
 
         void WriteProfile(const ProfileResult& result)//写入测试结果
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;//创建一个字符串流
 
             std::string name = result.Name;//测试的名字
             std::replace(name.begin(), name.end(), '"', '\'');//将双引号替换为单引号
 
-            //写入测试结果
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
-            
-            m_OutputStream.flush();//清空缓冲区
+            //将测试结果写入json字符串流中
+            json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.End - result.Start) << ',';
+            json << "\"name\":\"" << name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start;
+            json << "}";
+
+            std::lock_guard lock(m_Mutex);//加锁
+            //如果当前会话不为空
+            if (m_CurrentSession) {
+				m_OutputStream << json.str();//写入测试结果
+                m_OutputStream.flush();//清空缓冲区
+            }
         }
+
+        static Instrumentor& Get()//获取测试类的实例
+        {
+            static Instrumentor instance;
+            return instance;
+        }
+
+    private:
 
         void WriteHeader()//写入头部
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             m_OutputStream.flush();//清空缓冲区
         }
 
@@ -84,11 +111,17 @@ namespace Hazel {
             m_OutputStream.flush();//清空缓冲区
         }
 
-        static Instrumentor& Get()//获取测试类的实例
+        //Note:必须在已经加锁的情况下调用这个函数
+        void InternalEndSession()//结束当前会话
         {
-            static Instrumentor instance;
-            return instance;
+            if (m_CurrentSession) {
+                WriteFooter();//写入尾部
+                m_OutputStream.close();//关闭输出流
+                delete m_CurrentSession;//删除当前会话
+                m_CurrentSession = nullptr;
+            }
         }
+
     };
 
     class InstrumentationTimer//测试的计时器
@@ -113,8 +146,7 @@ namespace Hazel {
             long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();//开始时间转换
             long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();//结束时间转换
 
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());//获取线程ID
-            Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });//写入测试结果
+            Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id() });//获取线程ID并写入测试结果
 
             m_Stopped = true;
         }
