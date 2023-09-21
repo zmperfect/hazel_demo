520ad113df15d8282d8211b874b957454bd5eb46
请听下一题 <2286512681@qq.com>
2023-09-21 11:24:16 +0800

    自动推导可用的FUNCSIG定义

--- Hazel_demo/src/Hazel/Debug/Instrumentor.h
+++ Hazel_demo/src/Hazel/Debug/Instrumentor.h
@@ -127,10 +127,32 @@ namespace Hazel {
 
 #define HZ_PROFILE 1
 #if HZ_PROFILE
+
+    // Resolve which function signature macro will be used. Note that this only
+    // is resolved when the (pre)compiler starts, so the syntax highlighting
+    // could mark the wrong one in your editor!
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+#define HZ_FUNCSIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+#define HZ_FUNCSIG __PRETTY_FUNCTION__
+#elif defined(__FUNCSIG__)
+#define HZ_FUNCSIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define HZ_FUNCSIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+#define HZ_FUNCSIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#define HZ_FUNCSIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+#define HZ_FUNCSIG __func__
+#else
+#define HZ_FUNCSIG "HZ_FUNCSIG unknown!"
+#endif
+
 #define HZ_PROFILE_BEGIN_SESSION(name, filepath) ::Hazel::Instrumentor::Get().BeginSession(name, filepath)//开始一个会话
 #define HZ_PROFILE_END_SESSION() ::Hazel::Instrumentor::Get().EndSession()//结束一个会话
 #define HZ_PROFILE_SCOPE(name) ::Hazel::InstrumentationTimer timer##__LINE__(name);//创建一个测试计时器
-#define HZ_PROFILE_FUNCTION() HZ_PROFILE_SCOPE(__FUNCSIG__)//获取函数签名
+#define HZ_PROFILE_FUNCTION() HZ_PROFILE_SCOPE(HZ_FUNCSIG)//获取函数签名
 #else
 #define HZ_PROFILE_BEGIN_SESSION(name, filepath)
 #define HZ_PROFILE_END_SESSION()
--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 workspace "Hazel_demo"
-	architecture "x64"
+	architecture "x86_64"
 	startproject "Sandbox"
 
 	configurations
