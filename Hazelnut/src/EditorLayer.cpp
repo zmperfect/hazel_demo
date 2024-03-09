8b5da6d44e519627440d5e9520203d7a2d598afd
家仁 李 <2286512681@qq.com>
2024-03-10 02:26:29 +0800

    实现拖放内容浏览器中的文件来加载场景

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -14,6 +14,8 @@
 
 namespace Hazel {
 
+    extern const std::filesystem::path g_AssetPath;
+
     EditorLayer::EditorLayer()
         : Layer("EditorLayer"), m_CameraController(1280.0f / 720.0f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
     {
@@ -279,6 +281,18 @@ namespace Hazel {
         uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID(0);//获取纹理ID
         ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });//显示纹理
 
+        // 拖放
+        if (ImGui::BeginDragDropTarget())
+		{
+			if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM")) //接受拖放的数据
+			{
+				const wchar_t* path = (const wchar_t*)payload->Data;
+				OpenScene(std::filesystem::path(g_AssetPath) / path);
+			}
+			ImGui::EndDragDropTarget();
+		}
+
+
         //Gizmos
         Entity selectedEntity = m_SceneHierarchyPanel.GetSelectedEntity();//获取选中的实体
         if (selectedEntity && m_GizmoType != -1)
@@ -425,14 +439,17 @@ namespace Hazel {
     {
         std::string filepath = FileDialogs::OpenFile("Hazel Scene (*.hazel)\0*.hazel\0");//按照过滤打开后缀为.hazel的文件
         if (!filepath.empty())
-        {
-            m_ActiveScene = CreateRef<Scene>();
-            m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-            m_SceneHierarchyPanel.SetContext(m_ActiveScene);//设置场景层次面板的上下文
+            OpenScene(filepath);
+    }
 
-            SceneSerializer serializer(m_ActiveScene);//场景序列器存储场景
-            serializer.Deserialize(filepath);//反序列化
-        }
+    void EditorLayer::OpenScene(const std::filesystem::path& path)
+    {
+        m_ActiveScene = CreateRef<Scene>();
+        m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+        m_SceneHierarchyPanel.SetContext(m_ActiveScene);//设置场景层次面板的上下文
+
+        SceneSerializer serializer(m_ActiveScene);//场景序列器存储场景
+        serializer.Deserialize(path.string());//反序列化
     }
 
     void EditorLayer::SaveSceneAs()
--- Hazelnut/src/Panels/ContentBrowserPanel.cpp
+++ Hazelnut/src/Panels/ContentBrowserPanel.cpp
@@ -6,10 +6,10 @@
 namespace Hazel {
 
     // 一旦有了项目，就改变这个路径
-    static const std::filesystem::path s_AssetPath = "assets";
+    extern const std::filesystem::path g_AssetPath = "assets";
 
     ContentBrowserPanel::ContentBrowserPanel()
-        : m_CurrentDirectory(s_AssetPath)
+        : m_CurrentDirectory(g_AssetPath)
     {
         m_DirectoryIcon = Texture2D::Create("Resources/Icons/ContentBrowser/DirectoryIcon.png");
         m_FileIcon = Texture2D::Create("Resources/Icons/ContentBrowser/FileIcon.png");
@@ -21,7 +21,7 @@ namespace Hazel {
         ImGui::Begin("Content Browser"); // 开始一个新的 ImGui 窗口，标题为 "Content Browser"
 
         // 如果当前目录不是 assets 目录，显示"<-"按钮，点击后将当前目录设置为其父目录，实现返回上一级目录的功能
-        if (m_CurrentDirectory != std::filesystem::path(s_AssetPath))
+        if (m_CurrentDirectory != std::filesystem::path(g_AssetPath))
         {
             if (ImGui::Button("<-"))
             {
@@ -45,11 +45,24 @@ namespace Hazel {
         for (auto& directoryEntry : std::filesystem::directory_iterator(m_CurrentDirectory))
         {
             const auto& path = directoryEntry.path();
-            auto relativePath = std::filesystem::relative(path, s_AssetPath);
+            auto relativePath = std::filesystem::relative(path, g_AssetPath);// 获取相对路径
             std::string filenameString = relativePath.filename().string();
 
+            ImGui::PushID(filenameString.c_str());// 设置按钮的 ID
             Ref<Texture2D> icon = directoryEntry.is_directory() ? m_DirectoryIcon : m_FileIcon;//设置图标
+            ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0, 0, 0, 0 });// 设置按钮的颜色
             ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });//设置按钮
+
+            // 拖放
+            if (ImGui::BeginDragDropSource())
+			{
+				const wchar_t* itemPath = relativePath.c_str();
+				ImGui::SetDragDropPayload("CONTENT_BROWSER_ITEM", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t));// 设置拖放的数据
+				ImGui::EndDragDropSource();
+			}
+
+			ImGui::PopStyleColor();
+
             if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
             {
                 if (directoryEntry.is_directory())
@@ -58,6 +71,8 @@ namespace Hazel {
             ImGui::TextWrapped(filenameString.c_str());//设置文本
 
             ImGui::NextColumn();
+
+            ImGui::PopID();
         }
 
         ImGui::Columns(1);// 设置列数为1，结束列布局
